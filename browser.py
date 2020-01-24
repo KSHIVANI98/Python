779437a0f9d25db47d74575f461af75034c0e74f
KSHIVANI98 <60178664+KSHIVANI98@users.noreply.github.com>
2020-01-24 17:25:30 +0530

    Add files via upload

---
+++ browser.py
@@ -0,0 +1,9 @@
+import pyautogui
+import time
+
+pyautogui.doubleClick(28,452)
+time.sleep(2)
+
+pyautogui.typewrite("https://od72880.us-east-2.aws.snowflakecomputing.com/")
+pyautogui.typewrite(["ENTER"])
+
